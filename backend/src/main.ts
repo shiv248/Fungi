418fcdf014db0919be11c9a141a614b498d17558
Shiv Gupta <sgupta35@ucmerced.edu>
2022-09-03 21:58:24 -0700

    setup backend serving static frontend

---
+++ backend/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  await app.listen(3000);
+  //console.log(`Server running on http://localhost:${process.env.SERVER_PORT}`, 'Bootstrap');
+  console.log(`Application is running on: ${await app.getUrl()}`);
+}
+bootstrap();
